3de586f30320b5dcf10e24d3b0265d121bdb905d
정필성 <ibetter.kr@gmail.com>
2025-03-01 16:59:37 +0900

    코드 수정

--- src/main/java/com/example/spring/posts/PostsController.java
+++ src/main/java/com/example/spring/posts/PostsController.java
@@ -2,8 +2,8 @@ package com.example.spring.posts;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -13,19 +13,21 @@ public class PostsController {
     PostsService postsService;
 
     // 게시글 등록 (화면, GET)
-    @GetMapping("/posts/create")
+    @RequestMapping(value = "/posts/create", method = RequestMethod.GET)
     public String create() {
         return "posts/create";
     }
 
     // 게시글 등록 (처리, POST)
-    @PostMapping("/posts/create")
+    @RequestMapping(value = "/posts/create", method = RequestMethod.POST)
     public String createPost(PostsVo postsVo, RedirectAttributes redirectAttributes) {
         boolean created = postsService.create(postsVo);
+
         if (created) {
             redirectAttributes.addFlashAttribute("successMessage", "게시글이 등록되었습니다.");
             return "redirect:/posts/";
         }
+        
         redirectAttributes.addFlashAttribute("errorMessage", "게시글 등록에 실패했습니다.");
         return "redirect:/posts/create";
     }
